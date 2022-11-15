3a6aa3623ea224a5d78bdf01739e6fe3ed4e438c
Eliton Cruvinel <eliton@netscape.net>
2022-11-15 11:13:49 -0300

    Final

--- src/main/java/com/devsuperior/bds03/config/ResourceServerConfig.java
+++ src/main/java/com/devsuperior/bds03/config/ResourceServerConfig.java
@@ -24,11 +24,8 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 	
 	private static final String[] PUBLIC = { "/oauth/token", "/h2-console/**" };
 	
-	private static final String[] OPERATOR_OR_ADMIN = { "/products/**", "/categories/**" };
+	private static final String[] OPERATOR_GET = { "/departments/**", "/employees/**" };
 	
-	private static final String[] ADMIN = { "/users/**" };
-	
-
 	@Override
 	public void configure(ResourceServerSecurityConfigurer resources) throws Exception {
 		resources.tokenStore(tokenStore);
@@ -44,9 +41,7 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
 
 		http.authorizeRequests()
 		.antMatchers(PUBLIC).permitAll()
-		.antMatchers(HttpMethod.GET, OPERATOR_OR_ADMIN).permitAll()
-		.antMatchers(OPERATOR_OR_ADMIN).hasAnyRole("OPERATOR", "ADMIN") // No banco de dados está com o prefixo ROLE_
-		.antMatchers(ADMIN).hasRole("ADMIN")
-		.anyRequest().authenticated();
+		.antMatchers(HttpMethod.GET, OPERATOR_GET).hasAnyRole("OPERATOR", "ADMIN")
+		.anyRequest().hasAnyRole("ADMIN");
 	}
 }
